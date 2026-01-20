c3b45c40f477460ccd4d2d6233e5a63fe4cc5e68
Pavel Ershov <devnulpavel@gmail.com>
2026-01-20 13:41:03 +0300

    Research

--- tdlib/build.rs
+++ tdlib/build.rs
@@ -35,7 +35,45 @@ fn load_tl(file: &str) -> io::Result<Vec<Definition>> {
 fn main() -> std::io::Result<()> {
     // Prevent linking libraries to avoid documentation failure
     #[cfg(not(feature = "dox"))]
-    system_deps::Config::new().probe().unwrap();
+    {
+        system_deps::Config::new().probe().unwrap();
+        
+        /* let dependencies = system_deps::Config::new().probe().unwrap();
+
+        // Документации:
+        // - https://doc.rust-lang.org/cargo/reference/build-scripts.html#rustc-link-lib
+
+        // Способы запуска сборки для подробного просмотра значений
+        // - cargo build -v
+        // - cargo build -vv
+
+        // Отладочный вывод:
+        // for library_path in dependencies.all_link_paths() {
+        //     println!("cargo::warning='Library path: {}'", library_path.display());
+        // }
+        // for library_name in dependencies.all_libs() {
+        //     println!("cargo::warning='Library name: {}'", library_name);
+        // }
+
+        // // Явное указание полного пути к библиотекам для линковки
+        // for library_path in dependencies.all_link_paths() {
+        //     for library_name in dependencies.all_libs() {
+        //         println!(
+        //             "cargo::rustc-link-lib={}/{}",
+        //             library_path.display(),
+        //             library_name
+        //         );
+        //     }
+        // }
+
+        // Отдельно указание с версией сборки
+        for (_, info) in dependencies.iter() {
+            for path in info.link_paths.iter() {
+                println!("cargo::rustc-link-search={}", path.display());
+            }
+            println!("cargo::rustc-link-lib={}.{}", info.name, info.version);
+        } */
+    }
 
     let definitions = load_tl("tl/api.tl")?;
 
